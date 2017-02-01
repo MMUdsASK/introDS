0a47b8948e7e678403a3022f2d62f17739efe1b8
MMUdsASK <menubearer@gmail.com>
2017-02-02 07:32:48 +0800

    Stuck on feature selection

--- partB/work1.py
+++ partB/work1.py
@@ -5,14 +5,21 @@ Created on Wed Jan 25 13:50:03 2017
 @author: admin
 """
 
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style="whitegrid", color_codes=True)
+
 
 data = pd.read_csv('LoansTrainingSet.csv')
 datacols = pd.Series(data.columns.values)
 
+#-------------------------------
 # DATA CLEANING PHASE
+#-------------------------------
 
-# DUPLICATE DETECTED
+# duplicate data
 dataDuplicate = data[data.duplicated(keep=False)]
 
 # learnt that the duplicated records have the same exact attributes
@@ -21,10 +28,29 @@ del dataDuplicate
 # drop the duplicates
 data = data.drop_duplicates(subset="Loan ID")
 
-# FOUND NON-COMPLLIANT CREDIT SCORE
-# basd on original document
-# all outliers have a 0 at the end
+# found 999999 in current loan amount
+# set them to NaN so that analysis can be done on the valid values
+# fixing credit score
+def fix4(row):
+    if row['Current Loan Amount'] == 99999999:
+        val = np.NaN
+    else:
+        val = row['Current Loan Amount']
+    return val
+    
+data['Current Loan Amount'] = data.apply(fix4, axis=1)
 
+
+# finding invalid values in max open credit
+dataScan3 = pd.to_numeric(data['Maximum Open Credit'], errors='coerce')
+index = dataScan3.isnull()
+
+# only two observations, drop
+xx = data.loc[index]
+data = data.drop(xx.index)
+data['Maximum Open Credit'] = pd.to_numeric(data['Maximum Open Credit'])
+
+# fixing credit score
 def fix1(row):
     if row['Credit Score'] > 800:
         val = row['Credit Score']/10
@@ -44,27 +70,130 @@ def fix2(row):
     
 data['Purpose'] = data.apply(fix2, axis=1)
 
+# merging haveMortage to Home Mortgage
+def fix3(row):
+    if row['Home Ownership'] == 'HaveMortgage':
+        val = 'Home Mortgage'
+    else:
+        val = row['Home Ownership']
+    return val
+    
+data['Home Ownership'] = data.apply(fix3, axis=1)
+
+# creating ordered categorical for employment
+jobyears = list(data['Years in current job'].unique())
+jobyearsC = ['n/a', jobyears[0], jobyears[7],jobyears[4],
+             jobyears[3],
+             jobyears[9], jobyears[8], jobyears[6], jobyears[5],
+             jobyears[10],jobyears[2],jobyears[1]]
+
+data['Years in current job'] = pd.Categorical(data['Years in current job'],
+                                categories = jobyearsC, ordered = True)
+
+# getting a workable data on debt
+debt = data['Monthly Debt']
+debt = debt.str.split('$',1)
+data1 = [x[1] for x in debt]
+data1 =[float(x.replace(',','')) for x in data1]
+data['Monthly Debt Fixed'] = data1 # lost the cent, don't care
 
 
-# GET CLASS DISTRIBUTION
+
+#-------------------------------
+# DATA ANALYSIS PHASE
+#-------------------------------
+
+# get column names
+colnames = list(data.columns.values)
+
+# something
+data.loc[(data['Current Loan Amount'] != 99999999
+)].groupby('Loan Status').hist(alpha=0.4, column='Current Loan Amount')
+
+
+# get class distribution
 data['Loan Status'].value_counts().plot(kind='bar')
 
 
-# DATA ANALYSIS PHASE
+# mapping employment
+sns.countplot(x="Years in current job", data=data, hue = "Loan Status", palette="Greens_d");
+# SAME SHAPE, NO CATEGORIES THAT DEFY CLASS DISTRIBUTION
+
+# boxplot credit score
+sns.boxplot(y="Credit Score", x="Loan Status", data=data);
+# some overlap, but there are median differences between the loan status
+
+
 # credit score, group by loan status
-data.groupby('Loan Status').hist(column='Credit Score')
+data.loc[(data['Credit Score'] < 650
+)].groupby('Loan Status').hist(column='Credit Score')
 
-# GET DISTINCT VALUES OF MANY COLUMNS:
-colnames = list(data.columns.values)
+# mapping credit score
+sns.countplot(x="Credit Score", data=data.loc[(data['Credit Score'] < 650
+)], hue = "Loan Status", palette="Greens_d")
 
-# YEARS IN CURRENT JOB
-jobyears = list(data['Years in current job'].unique())
+
+
+# tax lien, group by loan status
+data.groupby('Loan Status').hist(column='Tax Liens')
+
+# credit score, group by loan status
+data.loc[(data['Tax Liens'] < 2
+)].groupby('Loan Status').hist(column='Tax Liens')
+
+# mapping employment
+sns.countplot(x="Home Ownership", data=data, hue = "Loan Status", palette="Greens_d");
+
+
+
+
 # HOME OWNERSHIP
 home = list(data['Home Ownership'].unique())
 # PURPOSE
 purpose = list(data['Purpose'].unique())
 
+# scatter matrix of all numeric attributes
+sns.pairplot(data, hue="Loan Status", diag_kind="kde")
+plt.savefig('out.png')
+
+# scatter matrix for charged off
+sns.pairplot(data.loc[(data['Loan Status'] != 'Fully Paid')], hue="Loan Status", diag_kind="kde")
+plt.savefig('out1.png')
+
+numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+numData = data._get_numeric_data()
+numData['Loan Status'] = data['Loan Status']
+sns.kdeplot(numData, hue = "Loan Status")
+
+
+# density plot
+data.plot(kind='density', subplots=True, sharex=False)
+
+# moderate correlation between credit problem and tax liens
+data[['Tax Liens','Number of Credit Problems']].corr()
+
+
+
+
+
+
+
 
 
 # MAXIMUM OPEN CREDIT HAS DATATYPE PROBLEM
 
+# RANDOM SCATTER
+data.loc[(data['Loan Status'] == 'Fully Paid')].plot.scatter(x='Annual Income',
+y='Credit Score',color='DarkBlue')
+
+data.plot.scatter(x='Monthly Debt Fixed',
+y='Credit Score',color='DarkBlue')
+
+data.loc[(data['Loan Status'] != 'Fully Paid')].plot.scatter(x='Monthly Debt Fixed',
+y='Credit Score',color='DarkBlue')
+
+data.plot.scatter(x='Monthly Debt Fixed',
+y='Credit Score',color='DarkBlue')
+
+
+# found only two observation, will remove; there's 100,000 total observations
